3a48aa46891c6e550b05048496f373cc5b78c160
Bhart <itsmebhsh@gmail.com>
2026-03-16 20:33:38 +0530

    second  class

---
+++ Conditionals02.py
@@ -0,0 +1,25 @@
+# If elif , else  (Conditional Statements)
+
+# age = int (input("Enter your Age : "))
+
+# if age>=18:
+#     print("You Can Vote")
+#     print("You Can Drive")
+
+# else:
+#     print("You cant vote")  
+# 
+
+
+
+
+color = input("Enter Color : ")
+
+if color == "red":
+    print("Stop Now")
+elif color == "yellow":
+    print("Slow Move")
+elif color == "green":
+    print("You Can Go")    
+else:
+    print("Invalid color")    
