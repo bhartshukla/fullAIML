b0d36e4e2e4d11714a7370d7a1bc84ed95190fea
Bhart <itsmebhsh@gmail.com>
2026-03-22 20:44:30 +0530

    name changed

---
+++ 03whileloops.py
@@ -0,0 +1,32 @@
+# loops start
+
+# WHILE , FOR
+
+# while True:
+#     print("Hello world")
+
+
+# counter = 1   #iterator
+
+# while counter <=5:
+#     print("hello Bharat", counter)
+#     counter+=1
+
+
+
+# count 1 to 5
+
+# count = 1
+
+# while count<=10:
+#     print(count)
+#     count+=1
+
+
+# print reverse 1 to 10    
+
+count = 10
+
+while count>=1:
+    print(count)
+    count-=1
