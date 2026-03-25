90198ea810969d7d2104193af9e5b69a183d34da
Bhart <itsmebhsh@gmail.com>
2026-03-25 22:46:31 +0530

    For loop in python

--- 04Forloops.py
+++ 04Forloops.py
@@ -25,6 +25,7 @@ print("Count of a is : ", count)
 
 '''
 
+'''
 # print vowel count of a given String
 
 word = "BharatShuklajiis a good boy"
@@ -35,4 +36,31 @@ for ch in word:
         print(ch , count)
 print(count, "these numbers of vowel")      
 
+'''
+
+# range function range(start, stop, step)
+
+'''
+
+for r in range(6):
+    print(r) 
+
+
+for r in range(1,6):
+    print(r)
+
+
+for r in range(1,6,2):
+    print(r)
+
+    '''
+
+
+# print the sum of n naturan number
+
+sum = 0
+
+for num in range(11):
+    sum = sum+num
+    print (num, "its sum is :", sum)
 
