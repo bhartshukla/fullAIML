c4ab9177d4e8468dea8730f13cc783b4852296dc
Bhart <itsmebhsh@gmail.com>
2026-04-06 20:55:33 +0530

    string

---
+++ 06string.py
@@ -0,0 +1,14 @@
+# string in puthon is immutable
+
+word ="Hello String"
+word2 = "in pyhton"
+# print(len(word))
+
+#concatnate
+# print(word + " " + word2)
+
+# print(word[2])
+
+for ch in word:
+    print(ch)
+
