c4b3c3cede53535b36a6f390c0b0e44b4ccae46d
Bhart <itsmebhsh@gmail.com>
2026-04-07 16:17:21 +0530

    list

---
+++ 07list.py
@@ -0,0 +1,16 @@
+# LIST (Mutable sequence of values)
+
+marks_list =[89,90,89,87,96,45]
+
+print(marks_list)
+# print(len(marks_list))
+
+# print(marks_list[4])
+
+marks_list[3] = 99
+
+print(marks_list)
+
+# slicing ----
+
+print(marks_list[1:5])
