84dcb8e352ac1cc835e62fd5bb614794b2b32934
Bhart <itsmebhsh@gmail.com>
2026-04-22 20:24:06 +0530

    today learning

---
+++ fundamaental py/10sets.py
@@ -0,0 +1,48 @@
+# Collection of unique elements
+
+# python set can be mutable but set element cant be mutable
+
+s = {1,2,3,4,2,2,4,}
+
+# print(s)
+# print(len(s))
+# print(type(s))
+
+
+# s.add(6)
+# print(s)
+
+# empty_set = set()
+
+
+# set methods ====================================
+
+# s.add(val)  adds a Value
+
+# s.remove(val)   remove a value
+
+# s.clear()    empties the set
+
+# s.pop()     removes a random Value
+
+# s.union(set2)  return new union 
+
+# s.intersection(set2)  return new intersection
+
+
+# s.remove(2)
+
+# print(s)
+
+# s.pop()
+# print(s)
+
+# s.clear()
+
+# print(s)
+
+
+
+s2 = {4,2,5,6,7,8}
+print(s.union(s2))
+print(s.intersection(s2))
